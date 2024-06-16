14ace7214766d83b475559a0d311d61b00e48891
Guilherme <111528193+guihi1@users.noreply.github.com>
2024-06-16 11:25:34 -0300

    Initial commit

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,42 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Dark Pastel'
+config.window_background_opacity = 0.7
+config.font = wezterm.font 'JetBrains Mono'
+config.font_size = 16
+-- config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+  {
+  key = "LeftArrow",
+  mods = "CTRL|SHIFT",
+  action = wezterm.action.ActivateTabRelative(-1),
+  },
+  {
+  key = "RightArrow",
+  mods = "CTRL|SHIFT",
+  action = wezterm.action.ActivateTabRelative(1),
+  },
+  {
+  key = "q",
+  mods = "CTRL|SHIFT",
+  action = wezterm.action.CloseCurrentTab{confirm=true},
+  },
+}
+config.window_padding = {
+  left = '0.5cell',
+  right = '0.5cell',
+  top = '0',
+  bottom = '0',
+}
+
+config.default_prog = { "/bin/bash" }
+
+-- and finally, return the configuration to wezterm
+return config
